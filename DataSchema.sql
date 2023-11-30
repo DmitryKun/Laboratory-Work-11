3b0ace932bd57c4b43f08164f48d9ff38a52c920
DmitryKun <101560464+DmitryKun@users.noreply.github.com>
2023-11-30 16:43:39 +0200

    Update DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,38 +1,46 @@
 CREATE TABLE "User" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(25),
-    dateOfBirth DATE,
+    dateofbirth DATE,
     email VARCHAR(50) UNIQUE,
     login VARCHAR(50),
     password VARCHAR(50),
     status VARCHAR(15) CHECK (status IN ('Active', 'Inactive', 'Blocked'))
 );
 
-CREATE TABLE Friend (
+CREATE TABLE friend (
     id SERIAL PRIMARY KEY,
     name VARCHAR(25),
-    dateOfBirth DATE,
+    dateofbirth DATE,
     email VARCHAR(50),
     status VARCHAR(15) CHECK (status IN ('Friend', 'Pending', 'Blocked')),
-    userId INTEGER REFERENCES "User" (id)
+    userid INTEGER REFERENCES "User" (id)
 );
 
-CREATE TABLE Settings (
+CREATE TABLE settings (
     id SERIAL PRIMARY KEY,
     background INTEGER,
     font INTEGER,
-    userId INTEGER REFERENCES "User" (id)
+    userid INTEGER REFERENCES "User" (id)
 );
 
-CREATE TABLE Chat (
-    firstUser INTEGER REFERENCES "User" (id),
-    secondUser INTEGER REFERENCES "User" (id)
+CREATE TABLE chat (
+    firstuser INTEGER REFERENCES "User" (id),
+    seconduser INTEGER REFERENCES "User" (id)
 );
 
 ALTER TABLE "User" ADD CONSTRAINT unique_email UNIQUE (email);
-ALTER TABLE "User" ADD CONSTRAINT check_name_characters CHECK (name ~ '^[A-Za-z]+$');
-ALTER TABLE "User" ADD CONSTRAINT check_email_format CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$');
+ALTER TABLE "User" ADD CONSTRAINT check_name_characters CHECK (
+    name ~ '^[A-Za-z]+$'
+);
+ALTER TABLE "User" ADD CONSTRAINT check_email_format CHECK (
+    email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$'
+);
 
-ALTER TABLE Friend ADD CONSTRAINT unique_user_email UNIQUE (userId, email);
-ALTER TABLE Friend ADD CONSTRAINT check_name_characters CHECK (name ~ '^[A-Za-z]+$');
-ALTER TABLE Friend ADD CONSTRAINT check_email_format CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$');
+ALTER TABLE friend ADD CONSTRAINT unique_user_email UNIQUE (userid, email);
+ALTER TABLE friend ADD CONSTRAINT check_name_characters CHECK (
+    name ~ '^[A-Za-z]+$'
+);
+ALTER TABLE friend ADD CONSTRAINT check_email_format CHECK (
+    email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$'
+);
