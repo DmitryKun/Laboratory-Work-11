15d388d4a86706117c9528898ca18e3b8628e99b
DmitryKun <101560464+DmitryKun@users.noreply.github.com>
2023-11-30 16:40:16 +0200

    Update DataSchema.sql

--- DataSchema.sql
+++ DataSchema.sql
@@ -1,40 +1,38 @@
-CREATE TABLE User(
-id BIGSERIAL PRIMARY KEY,
-name VARCHAR(25),
-dateOfBirth VARCHAR(25),
-email VARCHAR(50),
-login VARCHAR(50),
-password VARCHAR(50),
-status VARCHAR(15)
+CREATE TABLE "User" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(25),
+    dateOfBirth DATE,
+    email VARCHAR(50) UNIQUE,
+    login VARCHAR(50),
+    password VARCHAR(50),
+    status VARCHAR(15) CHECK (status IN ('Active', 'Inactive', 'Blocked'))
 );
 
-CREATE TABLE Friend(
-id BIGSERIAL PRIMARY KEY,
-name VARCHAR(25),
-dateOfBirth VARCHAR(25),
-email VARCHAR(50),
-status VARCHAR(15),
-userId INTEGER FOREIGN KEY (userId) REFERENCES User (id)
+CREATE TABLE Friend (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(25),
+    dateOfBirth DATE,
+    email VARCHAR(50),
+    status VARCHAR(15) CHECK (status IN ('Friend', 'Pending', 'Blocked')),
+    userId INTEGER REFERENCES "User" (id)
 );
 
-CREATE TABLE Settings(
-id BIGSERIAL PRIMARY KEY,
-background INTEGER,
-font INTEGER,
-userId INTEGER FOREIGN KEY (User) REFERENCES User (id)
+CREATE TABLE Settings (
+    id SERIAL PRIMARY KEY,
+    background INTEGER,
+    font INTEGER,
+    userId INTEGER REFERENCES "User" (id)
 );
 
-CREATE TABLE Chat(
-firstUser INTEGER FOREIGN KEY (User) REFERENCES User (id),
-secondUser INTEGER FOREIGN KEY (User) REFERENCES User (id)
+CREATE TABLE Chat (
+    firstUser INTEGER REFERENCES "User" (id),
+    secondUser INTEGER REFERENCES "User" (id)
 );
 
-ALTER TABLE User ADD CONSTRAINT unique_email UNIQUE (email);
-ALTER TABLE User ADD CONSTRAINT check_status CHECK (status IN ('Active', 'Inactive', 'Blocked'));
+ALTER TABLE "User" ADD CONSTRAINT unique_email UNIQUE (email);
+ALTER TABLE "User" ADD CONSTRAINT check_name_characters CHECK (name ~ '^[A-Za-z]+$');
+ALTER TABLE "User" ADD CONSTRAINT check_email_format CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$');
+
 ALTER TABLE Friend ADD CONSTRAINT unique_user_email UNIQUE (userId, email);
-ALTER TABLE Friend ADD CONSTRAINT check_status CHECK (status IN ('Friend', 'Pending', 'Blocked'));
-
-ALTER TABLE User ADD CONSTRAINT check_name_characters CHECK (name ~ '^[A-Za-z]+$');
 ALTER TABLE Friend ADD CONSTRAINT check_name_characters CHECK (name ~ '^[A-Za-z]+$');
-ALTER TABLE User ADD CONSTRAINT check_email_format CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$');
 ALTER TABLE Friend ADD CONSTRAINT check_email_format CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$');
