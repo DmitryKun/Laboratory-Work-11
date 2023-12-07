4241010cf4ef7b81cf46477d16fac40d2afc0c2f
DmitryKun <101560464+DmitryKun@users.noreply.github.com>
2023-12-07 16:14:12 +0200

    Update TestCase.java

--- TestCase.java
+++ TestCase.java
@@ -1,32 +1,32 @@
 import org.junit.jupiter.api.Test; 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestCases {
+public class SettingsTest {
 
     @Test
     public void testChangeStatus_WhenUserIDIsNull_ReturnsMinusOne() {
-        ProgramCode settings = new PraogramCode();
+        Settings settings = new Settings();
         int result = settings.changeStatus(null, "online");
         assertEquals(-1, result);
     }
 
     @Test
     public void testChangeStatus_WhenUserIDIsEmpty_ReturnsMinusOne() {
-        ProgramCode settings = new PraogramCode();
+        Settings settings = new Settings();
         int result = settings.changeStatus("", "offline");
         assertEquals(-1, result);
     }
 
     @Test
     public void testChangeStatus_WhenNewStatusIsInvalid_ReturnsMinusTwo() {
-        ProgramCode settings = new PraogramCode();
+        Settings settings = new Settings();
         int result = settings.changeStatus("user123", "invalid_status");
         assertEquals(-2, result);
     }
 
     @Test
     public void testChangeStatus_WhenNewStatusIsOnline_UpdatesStatusAndReturnsOne() {
-        ProgramCode settings = new PraogramCode();
+        Settings settings = new Settings();
         int result = settings.changeStatus("user123", "online");
         assertEquals(1, result);
         assertEquals("online", settings.getStatus());
@@ -34,10 +34,9 @@ public class TestCases {
 
     @Test
     public void testChangeStatus_WhenNewStatusIsOffline_UpdatesStatusAndReturnsOne() {
-        ProgramCode settings = new PraogramCode();
+        Settings settings = new Settings();
         int result = settings.changeStatus("user123", "offline");
         assertEquals(1, result);
         assertEquals("offline", settings.getStatus());
     }
 } 
-
