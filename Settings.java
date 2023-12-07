f3827df578ffed6c06442128b4e3cdc378a6fa1b
DmitryKun <101560464+DmitryKun@users.noreply.github.com>
2023-12-07 16:14:55 +0200

    Create Settings.java

---
+++ Settings.java
@@ -0,0 +1,27 @@
+public class Settings {
+    private String status;
+
+    public int changeStatus(String userID, String newStatus) {
+        if (userID == null || userID.isEmpty()) {
+            return -1;
+        }
+
+        if (!newStatus.equals("online") && !newStatus.equals("offline")) {
+            return -2;
+        }
+
+        if (newStatus.equals("online")) {
+            this.status = "online";
+            System.out.println("Status changed to online for user with ID: " + userID);
+        } else {
+            this.status = "offline";
+            System.out.println("Status changed to offline for user with ID: " + userID);
+        }
+
+        return 1;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
